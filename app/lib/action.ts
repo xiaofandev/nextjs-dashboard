7cae2bd65295dd747132b600679d521b7e10ba37
肖帆 <xiaofandev@126.com>
2024-01-19 21:52:53 +0800

    chapter 13 handling errors

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -27,10 +27,15 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+    try {
+        await sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+    } catch(e) {
+        return {message: 'Database error, insert invoice failed.'}
+    }
+    
     
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -46,17 +51,29 @@ export async function updateInvoice(id: string, formData: FormData) {
     const {customerId, amount, status} = UpdateInvoice.parse(rawFormData);
     const amountInCents = amount * 100;
 
-    await sql`
-        UPDATE invoices SET customer_id=${customerId}, amount=${amountInCents}, status=${status} 
-        WHERE ID=${id}
-    `;
-
+    try {
+        await sql`
+            UPDATE invoices SET customer_id=${customerId}, amount=${amountInCents}, status=${status} 
+            WHERE ID=${id}
+        `;
+    } catch (e) {
+        return {message: 'Database error, update invoice failed.'}
+    }
+    
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
 export async function deleteInvoice(id: string) {
-    await sql`
-        DELETE FROM invoices WHERE ID=${id}
-    `
-    revalidatePath('/dashboard/invoices');
+
+    try {
+        await sql`
+            DELETE FROM invoices WHERE ID=${id}
+        `
+        revalidatePath('/dashboard/invoices');
+        return {message: 'Invoice deleted.'}
+    } catch (e) {
+        return {message: 'Database error, delete invoice failed.'}
+    }
+    
+    
 }
