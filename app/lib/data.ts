7be091ec6b59a201a5f4a323dceaa052fb1bb84d
肖帆 <xiaofandev@126.com>
2024-01-20 17:58:18 +0800

    chapter 16 Adding Metadata

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -18,9 +18,9 @@ export async function fetchRevenue() {
 
   try {
     // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    // // Don't do this in production :)
+    // console.log('Fetching revenue data...');
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
